b404e685e87302d5687fc59267031353ce249da9
zeater <a3221464817@gmail.com>
2024-04-08 19:12:20 +0800

    add language function

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import render_template  
 from flask import Flask, request, jsonify
 from flask_cors import CORS  
@@ -11,11 +10,17 @@ import os
 
 app = Flask(__name__)
 CORS(app) 
-ocr_model = PaddleOCR(lang='en')#Traditional chinese
+app.ocr_model = PaddleOCR(lang='en')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/switch_lang', methods=['POST'])
+def switch_lang():
+    lang = request.form.get('lang', 'en')
+    app.ocr_model = PaddleOCR(lang=lang)  # 將新的語言模型保存到應用程序上下文中
+    return jsonify({'success': True})
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -27,12 +32,11 @@ def upload():
     if file.filename == '':
         return jsonify({'error': 'No file selected'})
 
-    # 直接處理上傳的圖片
-    result = process_image(file)
-
+    # 使用保存在應用程序上下文中的模型進行文字檢測
+    result = process_image(file, app.ocr_model)
     return jsonify({'result': result})
 
-def process_image(file):
+def process_image(file, ocr_model):
     # 將上傳的文件轉換成 OpenCV 格式的圖片
     nparr = np.frombuffer(file.read(), np.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
@@ -40,40 +44,12 @@ def process_image(file):
     # 使用 PaddleOCR 進行文字偵測
     result = ocr_model.ocr(img)
     texts = []
-
     for res in result:
         for item in res:
             texts.append(item[1][0])
     return texts
 
 
-
-
-
-@app.route('/en')
-def en():
-    ocr_model_en = PaddleOCR(lang='en')#Traditional chinese
-    return render_template('en.html')
-
-@app.route('/ch')
-def ch():
-    ocr_model_ch = PaddleOCR(lang='ch')#Traditional chinese
-    return render_template('ch.html')
-
-@app.route('/jp')
-def jp():
-    ocr_model_jp = PaddleOCR(lang='japan')#Traditional chinese
-    return render_template('jp.html')
-
-@app.route('/kr')
-def kr():
-    ocr_model_kr = PaddleOCR(lang='korean')#Traditional chinese
-    return render_template('kr.html')
-
-@app.route('/fr')
-def fr():
-    ocr_model_fr = PaddleOCR(lang='fr')#Traditional chinese
-    return render_template('fr.html')
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(host='0.0.0.0',port=4500,debug=True)
+
